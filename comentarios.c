2351eaf56fb276dd1b4553ec060424343a4a6507
Alisson Miranda <alissonmiranda1225@gmail.com>
2024-07-30 13:22:06 -0300

    comentarios em c

---
+++ comentarios.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	/*
+	 Comentarios para documentação
+	 
+	* */
+	
+	printf("Aprendendo comentarios na linguagem c\nOla mundo!");
+	
+	//printf("nao sera executado");
+	return 0;
+}
+
