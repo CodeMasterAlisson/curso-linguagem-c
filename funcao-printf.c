2064fbdb2e2d5b7a96be10744e0ef8ce9a1d241c
Alisson Miranda <alissonmiranda1225@gmail.com>
2024-07-30 13:32:25 -0300

    funcao printf

---
+++ funcao-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	//\n serve para pular de linha
+	printf("Olá, seja bem vindo!!\n");
+	printf("\n---------------------------------\n\n");
+	printf("1 - Logar	2 - Cadastrar	3 - Imprimir\n");
+	printf("\n---------------------------------\n\n");
+	
+	printf("\nValor retornado: %d", printf("Oi"));
+	
+	return 0;
+}
+
