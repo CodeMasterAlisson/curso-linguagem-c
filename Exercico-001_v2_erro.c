7bc007cfb5f430f058d6c3df0223d83a07013cf6
Alisson Miranda <alissonmiranda1225@gmail.com>
2024-07-30 21:55:18 -0300

    Exercicio-001_v2-buffer

--- Exercico-001_v2_erro.c
+++ Exercico-001_v2_erro.c
@@ -12,7 +12,9 @@ int main()
 	float peso, altura;
 	
 	printf("Digite idade, peso e altura, sexo (f, F, m ou M):\n");
-	scanf("%d%d%f%c", &idade, &idade, &peso, &sexo);
+	scanf("%d%f%f %c", &idade, &peso, &altura, &sexo); // deixando um espaco, é descosiderado um enter, espaco ou tab
+	
+	// Existe o buffer do teclado apos pular uma linha com o \n
 	
 	printf("Sexo: %c\nIdade: %d\nPeso: %.1f\nAltura: %.2f\n", sexo, idade, peso, altura);
 	
