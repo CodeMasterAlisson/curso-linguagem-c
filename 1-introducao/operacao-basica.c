b64da84ed9f45c7d3d9189a0c28bd7089374ad8b
Alisson Miranda <alissonmiranda1225@gmail.com>
2024-09-08 22:31:30 -0300

    operadores logicos

---
+++ 1-introducao/operacao-basica.c
@@ -0,0 +1,19 @@
+/*
+ * operacao-basica.c
+ * 
+ */
+
+/*
+ * 
+ * */
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{	
+	printf("\nSoma: %d\n", 10 + 20);
+	printf("\nSubtracao: %d\n", 10 - 20);
+	printf("\nMultiplicacao: %d", 10 * 20);
+	printf("\nDivisão: %.2f\n", 10.0/20);
+	return 0;
+}
+
