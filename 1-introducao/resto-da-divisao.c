13f58e04f03342a88e4d77f086d0f373bb5d9e63
Alisson Miranda <alissonmiranda1225@gmail.com>
2024-09-18 18:39:37 -0300

    operador de resto

---
+++ 1-introducao/resto-da-divisao.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Aula - Operador de resto da divisão entre dois numeros
+*/
+
+int main(){
+
+    int a = 7, b = 21;
+
+
+
+    printf("\nResto da divisao inteira: %d\n", b % 2);
+
+    return 0;
+}
