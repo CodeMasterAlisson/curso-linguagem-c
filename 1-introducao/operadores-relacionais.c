6098c7e91e499fcb3144e2c8dcb3d3efea35332f
Alisson Miranda <alissonmiranda1225@gmail.com>
2024-09-19 15:13:50 -0300

    operadores relacionais

---
+++ 1-introducao/operadores-relacionais.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    menor que <
+    maior que >
+    menor ou igual <=
+    maior ou igual <=
+    igual ==
+    diferente !=
+
+    true = 1
+    false = 0
+*/
+
+int main(){
+
+    int a = 10, b = 20;
+
+    printf("\n\tResultado: %d\n\n", a > b);
+    
+    return 0;
+}
